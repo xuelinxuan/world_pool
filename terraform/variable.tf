d7dece4f6c233272cf8007541c292e97df7add05
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-23 17:48:18 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -17,11 +17,11 @@ variable "alert_email_id"{default = "linxuanxue@gmail.com"}
 variable "lambda_function_name" {default = "IGTIexecutaEMRaovivo"}
 
 #SSH 
-variable "key_pair_name" {default = "ssh_ec2_instance"}
+#variable "key_pair_name" {default = "ssh_ec2_instance"}
 
 #subnet
-variable "airflow_subnet_id" {default = "subnet-08b465766ec396cde"}
+#variable "airflow_subnet_id" {default = "subnet-08b465766ec396cde"}
 
 #vpc
-variable "vpc_id" {default = "vpc-06767fed36ee2d510"}
+#variable "vpc_id" {default = "vpc-06767fed36ee2d510"}
 
