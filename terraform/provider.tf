020cd9a15425ffdcd6e13b33b30357081906f645
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-23 18:32:21 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  profile    = "default"
+  # profile    = "default" 就是已经把 acces ,secert 写到了cli 中，直接调取
 }
