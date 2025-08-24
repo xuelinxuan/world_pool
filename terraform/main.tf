4a25831fc3c20025cb8aba9788e6d2d646f5a4d5
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-24 17:37:13 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,15 @@ resource "aws_security_group" "sde_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # 8080/Airflow UI 入站（新增）
+  ingress {
+    description = "Airflow UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
