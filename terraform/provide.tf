62c681b7b9f8c4b7127c2c30bafaceb70cf49b81
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-25 23:38:03 +0200

    Update provide.tf

--- terraform/provide.tf
+++ terraform/provide.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region       = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key  #有了role就不需要这个两个
+  #secret_key = var.aws_secret_key  #有了role就不需要这个两个
   # profile    = "default" 就是已经把 acces ,secert 写到了cli 中，直接调取
 }
