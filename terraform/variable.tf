22fb9d42cf075169a353effc93371cb82304acfd
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-25 23:14:42 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -14,10 +14,10 @@ variable "alert_email_id"   {default = "linxuanxue@gmail.com"}
 variable "repo_url"         {default = "https://github.com/xuelinxuan/world_pool.git"}
 
 # AWS Access Key
-#variable "aws_access_key"   {default = "AKIAWDYU6IA6HRAXK7XW"}
+variable "aws_access_key"   {default = "AKIAWDYU6IA6HRAXK7XW"}
 
 # AWS Secret Key
-#variable "aws_secret_key"   {default = "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"}
+variable "aws_secret_key"   {default = "J8AOvV4C/JtXV+rYF8VqMa28RkHCYGw+AiC/PrD8"}
 
 
 
