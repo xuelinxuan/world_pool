9f606b8fbc322769563d1db5f4268b8d25c42c26
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-30 18:52:10 +0200

    Update extract_market_bronze.py

--- dags/extract_market_bronze.py
+++ dags/extract_market_bronze.py
@@ -55,37 +55,36 @@ def cb_currency_daily_raw():
     df = paras.cb_currency()
     save_bronze_parquet.save( df,'cb_currency_daily_raw')
     return df.shape[0]
+    
+def clean(filename):
+    S3=S3_save_extract("bronze", None)
+    return S3.extract(filename)
 
 with DAG(
-    dag_id='extract_market',             # DAG 名称（在 Airflow UI 里显示）
+    dag_id='market_pv',             # DAG 名称（在 Airflow UI 里显示）
     schedule_interval=None,  #手动  #'*/5 * * * *',  每 5 分钟运行一次  # 每天跑一次'@daily',   
     start_date=datetime(2023, 1, 1),        # DAG 生效开始日期
     catchup=False                           # 不补跑历史数据
 ) as dag:
 
-    start = EmptyOperator(task_id='start')
 
-    cb_market_history_raw_task   = PythonOperator(
-        task_id         ='cb_market_history_raw',
-        python_callable = cb_market_history_raw,
-        provide_context = True )
 
-    cb_market_daily_raw_task     = PythonOperator(
-        task_id         ='cb_market_daily_raw',
-        python_callable = cb_market_daily_raw,
-        provide_context =True )
+    cb_market_history_raw_task    = PythonOperator(task_id='cb_market_history_raw',         python_callable = cb_market_history_raw,  provide_context = True)
+    clean_cb_market_history_raw   = PythonOperator(task_id='clean_cb_market_history_raw',   python_callable = lambda: clean("cb_market_history_raw"))
+    
+    cb_market_daily_raw_task      = PythonOperator(task_id='cb_market_daily_raw',           python_callable = cb_market_daily_raw,    provide_context =True)
+    clean_cb_market_daily_raw     = PythonOperator(task_id='clean_cb_market_daily_raw',     python_callable = lambda: clean("cb_market_daily_raw"))
+   
+    cb_currency_history_raw_task  = PythonOperator(task_id='cb_currency_history_raw',       python_callable = cb_currency_history_raw,provide_context = True)
+    clean_cb_currency_history_raw = PythonOperator(task_id='clean_cb_currency_history_raw', python_callable = lambda: clean("cb_currency_history_raw"))
+    
+    cb_currency_daily_raw_task    = PythonOperator(task_id='cb_currency_daily_raw',         python_callable = cb_currency_daily_raw,  provide_context = True)
+    clean_cb_currency_daily_raw   = PythonOperator(task_id='clean_cb_currency_daily_raw',   python_callable = lambda: clean("cb_currency_daily_raw"))
 
-    cb_currency_history_raw_task = PythonOperator(
-        task_id         ='cb_currency_history_raw',
-        python_callable = cb_currency_history_raw,
-        provide_context = True )
-
-    cb_currency_daily_raw_task   = PythonOperator(
-        task_id         ='cb_currency_daily_raw',
-        python_callable = cb_currency_daily_raw,
-        provide_context = True )
-
-    end = EmptyOperator(task_id='end')
 
     # Set task dependencies
-    start >> [cb_market_history_raw_task, cb_market_daily_raw_task, cb_currency_history_raw_task, cb_currency_daily_raw_task]>> end
+    [cb_market_history_raw_task   >> clean_cb_market_history_raw,
+    cb_market_daily_raw_task     >> clean_cb_market_daily_raw,
+    cb_currency_history_raw_task >> clean_cb_currency_history_raw,
+    cb_currency_daily_raw_task   >> clean_cb_currency_daily_raw]
+
