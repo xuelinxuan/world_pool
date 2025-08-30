fbdf6398e1df331fb82419c1af4dfca8a29bba15
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-30 12:13:32 +0200

    Update extraire_market_history_bronze.py

--- dags/extraire_market_history_bronze.py
+++ dags/extraire_market_history_bronze.py
@@ -7,7 +7,6 @@ from   pyspark.sql                  import SparkSession, functions as F
 from   delta.tables                 import DeltaTable
 from   utils.market_pv              import yahoo_pv, SaveS3_niveau_format
 import requests, time, os,io
-
 import pandas as pd
 
 # from pyspark.sql import SparkSession
@@ -18,26 +17,8 @@ os.environ['NO_PROXY'] = '*'  #request 不用代理环境
 today = date.today().strftime("%Y-%m-%d")
 yesterday = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-
-# currency_history   =yahoo_pv(start='2020-01-01'   , end='2022-01-01', ticker='CNY=X'     , ticker_list =["CNY=X","EURCHF=X"])
-# currency_daily     =yahoo_pv(start=yesterday      , end=today       , ticker='CNY=X'     , ticker_list =["CNY=X","EURCHF=X"])
-# market_history     =yahoo_pv(start='2020-01-01'   , end='2022-01-01', ticker='510050.SS' , ticker_list =["SPY"  ,"510050.SS"])
-# market_daily       =yahoo_pv(start=yesterday      , end=today       , ticker='SPY'       , ticker_list =["SPY"  ,"510050.SS"])
 # # format_tppe:  parquet:None,snappy,gzip
 save_bronze_parquet=SaveS3_niveau_format(niveau="bronze",format=None)
-
-
-# cb_market_history_raw   = market_history.cb_market()
-# cb_market_history_raw   = save_bronze_parquet.name(cb_market_history_raw, "cb_market_history_raw")
-
-# cb_market_daily_raw     = market_daily.cb_market()
-# cb_market_daily_raw     = save_bronze_parquet.name(cb_market_daily_raw, "cb_market_daily_raw")
-
-# cb_currency_history_raw = currency_history.cb_currency()
-# cb_currency_history_raw = save_bronze_parquet.name(cb_currency_history_raw, "cb_currency_history_raw")
-
-# cb_currency_daily_raw   = currency_daily.cb_currency()
-# cb_currency_daily_raw   = save_bronze_parquet.name(cb_currency_daily_raw, "cb_currency_daily_raw")
 
 def cb_market_history_raw():
     paras = yahoo_pv(
@@ -75,8 +56,6 @@ def cb_currency_daily_raw():
     save_bronze_parquet.name(df, "cb_currency_daily_raw")
     return df.shape[0]
 
-
-
 with DAG(
     dag_id='bronze_market',             # DAG 名称（在 Airflow UI 里显示）
     schedule_interval=None,  #手动  #'*/5 * * * *',  每 5 分钟运行一次  # 每天跑一次'@daily',   
@@ -93,21 +72,20 @@ with DAG(
 
     cb_market_daily_raw_task     = PythonOperator(
         task_id         ='cb_market_daily_raw',
-        python_callable =cb_market_daily_raw,
+        python_callable = cb_market_daily_raw,
         provide_context =True )
 
     cb_currency_history_raw_task = PythonOperator(
         task_id         ='cb_currency_history_raw',
-        python_callable =cb_currency_history_raw,
-        provide_context =True )
+        python_callable = cb_currency_history_raw,
+        provide_context = True )
 
     cb_currency_daily_raw_task   = PythonOperator(
         task_id         ='cb_currency_daily_raw',
-        python_callable =cb_currency_daily_raw,
-        provide_context =True )
+        python_callable = cb_currency_daily_raw,
+        provide_context = True )
 
     end = EmptyOperator(task_id='end')
-
 
     # Set task dependencies
     start >> [cb_market_history_raw_task, cb_market_daily_raw_task, cb_currency_history_raw_task, cb_currency_daily_raw_task]>> end
