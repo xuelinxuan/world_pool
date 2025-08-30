fd431f4693533bab1c64c24e35823e250ccd9a7b
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-08-30 18:55:01 +0200

    Update extract_market_bronze.py

--- dags/extract_market_bronze.py
+++ dags/extract_market_bronze.py
@@ -21,37 +21,25 @@ yesterday = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 save_bronze_parquet=S3_save_extract(niveau="bronze",format=None)
 
 def cb_market_history_raw():
-    paras = yahoo_pv(
-        start='2020-01-01', end='2022-01-01',
-        ticker='510050.SS', ticker_list=['SPY', '510050.SS']
-    )
+        paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='510050.SS', ticker_list=['SPY', '510050.SS'])
     df = paras.cb_market()
     save_bronze_parquet.save(df,'cb_market_history_raw')
     return df.shape[0]
 
 def cb_market_daily_raw():
-    paras = yahoo_pv(
-        start=yesterday, end=today,
-        ticker='SPY', ticker_list=['SPY', '510050.SS']
-    )
+    paras = yahoo_pv(start=yesterday, end=today,ticker='SPY', ticker_list=['SPY', '510050.SS'])
     df = paras.cb_market()
     save_bronze_parquet.save(df,'cb_market_daily_raw')
     return df.shape[0]
 
 def cb_currency_history_raw():
-    paras = yahoo_pv(
-        start='2020-01-01', end='2022-01-01',
-        ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X']
-    )
+    paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X'])
     df = paras.cb_currency()
     save_bronze_parquet.save(df,'cb_currency_history_raw')
     return df.shape[0]
 
 def cb_currency_daily_raw():
-    paras = yahoo_pv(
-        start=yesterday, end=today,
-        ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X']
-    )
+    paras = yahoo_pv(start=yesterday, end=today,ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X'])
     df = paras.cb_currency()
     save_bronze_parquet.save( df,'cb_currency_daily_raw')
     return df.shape[0]
@@ -60,14 +48,7 @@ def clean(filename):
     S3=S3_save_extract("bronze", None)
     return S3.extract(filename)
 
-with DAG(
-    dag_id='market_pv',             # DAG 名称（在 Airflow UI 里显示）
-    schedule_interval=None,  #手动  #'*/5 * * * *',  每 5 分钟运行一次  # 每天跑一次'@daily',   
-    start_date=datetime(2023, 1, 1),        # DAG 生效开始日期
-    catchup=False                           # 不补跑历史数据
-) as dag:
-
-
+with DAG(dag_id='market_pv',   schedule_interval=None, start_date=datetime(2023, 1, 1),  catchup=False) as dag:
 
     cb_market_history_raw_task    = PythonOperator(task_id='cb_market_history_raw',         python_callable = cb_market_history_raw,  provide_context = True)
     clean_cb_market_history_raw   = PythonOperator(task_id='clean_cb_market_history_raw',   python_callable = lambda: clean("cb_market_history_raw"))
@@ -80,7 +61,6 @@ with DAG(
     
     cb_currency_daily_raw_task    = PythonOperator(task_id='cb_currency_daily_raw',         python_callable = cb_currency_daily_raw,  provide_context = True)
     clean_cb_currency_daily_raw   = PythonOperator(task_id='clean_cb_currency_daily_raw',   python_callable = lambda: clean("cb_currency_daily_raw"))
-
 
     # Set task dependencies
     [cb_market_history_raw_task   >> clean_cb_market_history_raw,
