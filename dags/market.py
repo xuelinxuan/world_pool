7026fc946c1ac18a3c555850f7f555b9366ef299
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-04 20:49:52 +0200

    Update market.py

--- dags/market.py
+++ dags/market.py
@@ -6,7 +6,7 @@ from   datetime                     import datetime,date, timedelta
 from   pyspark.sql                  import SparkSession, functions as F
 from   delta.tables                 import DeltaTable
 from   utils.market_pv              import market_currency, yahoo_pv, S3_save_extract
-import requests, time, os,io
+import requests, time, os, io
 import pandas as pd
 
 # from pyspark.sql import SparkSession
