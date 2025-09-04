7ec31f8fb66754a0f1d71018b950b90e23e6c62f
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-04 19:37:12 +0200

    Update market.py

--- dags/market.py
+++ dags/market.py
@@ -5,7 +5,7 @@ from   airflow.operators.empty      import EmptyOperator
 from   datetime                     import datetime,date, timedelta
 from   pyspark.sql                  import SparkSession, functions as F
 from   delta.tables                 import DeltaTable
-from   .utils.market_pv             import market_currency, yahoo_pv, S3_save_extract
+from   utils.market_pv              import market_currency, yahoo_pv, S3_save_extract
 import requests, time, os,io
 import pandas as pd
 
