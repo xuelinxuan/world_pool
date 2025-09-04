9164aa918a168bd9d2dcf35a44bd57c650b2dda9
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-04 18:39:12 +0200

    Update test_integrity.py

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -14,15 +14,13 @@ from airflow.utils.dag_cycle_tester import check_cycle
 DAG_PATH = "/home/runner/work/world_pool/world_pool/dags"
 DAG_FILES = [f for f in os.listdir(DAG_PATH) if f.endswith('.py')]
 
-
 @pytest.fixture()
 def dag_bag():
     return DagBag(dag_folder=DAG_PATH, include_examples=False)  #不加载 airflow 自带的exmple dags
 
-
 @pytest.mark.parametrize('dag_file', DAG_FILES)
 def test_dag_integrity(dag_file):
-    """Import dag files and check for DAG."""
+    """Import dag files and check for DAG."""  
     module_name, _ = os.path.splitext(dag_file)
     module_path    = os.path.join(DAG_PATH, dag_file)
     mod_spec       = importlib.util.spec_from_file_location(module_name, module_path)
