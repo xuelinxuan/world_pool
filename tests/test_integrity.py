679aa3d0e6cf09388d234ab940d237836665e64e
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-04 18:34:20 +0200

    Update test_integrity.py

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -24,12 +24,12 @@ def dag_bag():
 def test_dag_integrity(dag_file):
     """Import dag files and check for DAG."""
     module_name, _ = os.path.splitext(dag_file)
-    module_path = os.path.join(DAG_PATH, dag_file)
-    mod_spec = importlib.util.spec_from_file_location(module_name, module_path)
-    module = importlib.util.module_from_spec(mod_spec)
+    module_path    = os.path.join(DAG_PATH, dag_file)
+    mod_spec       = importlib.util.spec_from_file_location(module_name, module_path)
+    module         = importlib.util.module_from_spec(mod_spec)
     mod_spec.loader.exec_module(module)
 
-    dag_objects = [var for var in vars(module).values() if isinstance(var, af_models.DAG)]
+    dag_objects    = [var for var in vars(module).values() if isinstance(var, af_models.DAG)]
 
     for dag in dag_objects:
         check_cycle(dag)
