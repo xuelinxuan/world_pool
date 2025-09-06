62d3f49809387869341e6abf5e4eb9c0f5831d6a
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-06 15:09:38 +0200

    Update test_dependence.py

--- tests/test_dependence.py
+++ tests/test_dependence.py
@@ -24,9 +24,9 @@ def dag_bag():
 def test_dag_integrity(dag_file):
     """Import dag files and check for DAG."""
     module_name, _ = os.path.splitext(dag_file)
-    module_path = os.path.join(DAG_PATH, dag_file)
-    mod_spec = importlib.util.spec_from_file_location(module_name, module_path)
-    module = importlib.util.module_from_spec(mod_spec)
+    module_path    = os.path.join(DAG_PATH, dag_file)
+    mod_spec       = importlib.util.spec_from_file_location(module_name, module_path)
+    module         = importlib.util.module_from_spec(mod_spec)
     mod_spec.loader.exec_module(module)
 
     dag_objects = [var for var in vars(module).values() if isinstance(var, af_models.DAG)]
