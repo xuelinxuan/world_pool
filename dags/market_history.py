75f27788d6fcda9ac646a154ac0b29a5927a6aa9
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-12 08:03:23 +0200

    Update market_history.py

--- dags/market_history.py
+++ dags/market_history.py
@@ -22,14 +22,14 @@ save_silver_parquet=S3_save_extract("silver",format=None)
 def cb_market_history_raw():
     paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='510050.SS', ticker_list=['SPY', '510050.SS'])
     df    = paras.cb_market()
-    save_bronze_parquet.save(df,'cb_market_history_raw')
+    save_bronze_parquet.save_hist(df,'cb_market_history_raw')
     return df.shape[0]
   
 @task
 def cb_currency_history_raw():
     paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X'])
     df    = paras.cb_currency()
-    save_bronze_parquet.save(df,'cb_currency_history_raw')
+    save_bronze_parquet.save_hist(df,'cb_currency_history_raw')
     return df.shape[0]
   
 @task
@@ -38,7 +38,7 @@ def market_history_currency_callable():
     extract_cb_market_history_raw    =S3.extract('cb_market_history_raw')
     extract_cb_currency_history_raw  =S3.extract('cb_currency_history_raw')
     df                               =market_currency(extract_cb_market_history_raw,extract_cb_currency_history_raw)
-    save_silver_parquet.save(df,'market_history_currency')
+    save_silver_parquet.save_hist(df,'market_history_currency')
     return df.shape[0]
 
 with DAG(dag_id='market_history', schedule_interval=None, start_date=datetime(2023, 1, 1),  catchup=False) as dag:
