4ffb8e14d8024725be30dd8cc23d58a014c6989b
彻 <72044077+xuelinxuan@users.noreply.github.com>
2025-09-14 20:15:03 +0200

    Update market_history.py

--- dags/market_history.py
+++ dags/market_history.py
@@ -23,6 +23,7 @@ S3_silver=S3_save_extract("silver",format=None)
 def cb_market_hist_raw():
     paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='510050.SS', ticker_list=['SPY', '510050.SS'])
     df    = paras.cb_market()
+    
     save_bronze_parquet.save_hist(df,'cb_market_history_raw')
     return df.shape[0]
   
@@ -30,6 +31,7 @@ def cb_market_hist_raw():
 def cb_currency_hist_raw():
     paras = yahoo_pv(start='2020-01-01', end='2022-01-01',ticker='CNY=X', ticker_list=['CNY=X', 'EURCHF=X'])
     df    = paras.cb_currency()
+    print(df.head(3)) 
     save_bronze_parquet.save_hist(df,'cb_currency_history_raw')
     return df.shape[0]
   
